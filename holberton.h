a71797653411a627a2ee45615f8cefd5b5cc0cc5
edward0rtiz <931@holbertonschool.com>
2019-08-19 15:06:39 +0000

    [feature] include string and memory handler functions Created: aux_mem.c, aux_str.c, aux_str2.c and holberton.h

---
+++ holberton.h
@@ -0,0 +1,42 @@
+#ifndef _HOLBERTON_
+#define _HOLBERTON_
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <signal.h>
+
+
+
+/* aux_str functions */
+char *_strcat(char *dest, const char *src);
+char *_strcpy(char *dest, char *src);
+int _strcmp(char *s1, char *s2);
+char *_strchr(char *s, char c);
+int _strspn(char *s, char *accept);
+
+
+
+
+
+/* aux_mem.c */
+void _memcpy(void *newptr, void *ptr, unsigned int size);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+
+/* aux_str2.c */
+char *_strdup(const char *s);
+int _strlen(const char *s);
+int cmp_chars(char str[], const char *delim);
+char *_strtok(char str[], const char *delim);
+
+
+
+
+
+
+#endif
