51b2f54b8ed7f7a8e2cefb42c8ad1055e4cf9dd9
luischaparoc <lfchaparro@outlook.com>
2019-08-20 00:05:16 +0000

    [Feature]: adding entry point function Created: main.c

---
+++ main.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * main - Entry point
+ *
+ * @ac: argument count
+ * @av: argument vector
+ *
+ * Return: 0 on success.
+ */
+int main(int ac, char **av)
+{
+
+	shell_loop();
+	return (0);
+}
