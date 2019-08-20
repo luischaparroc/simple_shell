6e5c442b9276e86917d86fc0bde4393e5d959320
edward0rtiz <931@holbertonschool.com>
2019-08-20 01:42:06 +0000

    [feature] added functions for itoa for convert integer in string data types. created aux_stdlib.c works for aux for get_error.c

---
+++ aux_stdlib.c
@@ -0,0 +1,66 @@
+#include "holberton.h"
+
+/**
+ * get_len - Get the lenght of a number.
+ * @n: type int number.
+ * Return: Lenght of a number.
+ */
+int get_len(int n)
+{
+	unsigned int n1;
+	int lenght = 1;
+
+	if (n < 0)
+	{
+		lenght++;
+		n1 = n * -1;
+	}
+	else
+	{
+		n1 = n;
+	}
+	while (n1 > 9)
+	{
+		lenght++;
+		n1 = n1 / 10;
+	}
+
+	return (lenght);
+}
+/**
+ * aux_itoa - function converts int to string.
+ * @n: type int number
+ * Return: String.
+ */
+char *aux_itoa(int n)
+{
+	unsigned int n1;
+	int lenght = get_len(n);
+	char *buffer;
+
+	buffer = malloc(sizeof(char) * (lenght + 1));
+	if (buffer == 0)
+		return (NULL);
+
+	*(buffer + lenght) = '\0';
+
+	if (n < 0)
+	{
+		n1 = n * -1;
+		buffer[0] = '-';
+	}
+	else
+	{
+		n1 = n;
+	}
+
+	lenght--;
+	do {
+		*(buffer + lenght) = (n1 % 10) + '0';
+		n1 = n1 / 10;
+		lenght--;
+	}
+	while (n1 > 0)
+		;
+	return (buffer);
+}
